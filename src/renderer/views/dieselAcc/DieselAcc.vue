3ad20e8a9dc15a4917191ad2aec0d8d05862efc2
Binote <binote@BinotedeiMac.lan>
2018-11-07 23:18:08 +0800

    加油记账编辑对话框

--- src/renderer/views/dieselAcc/DieselAcc.vue
+++ src/renderer/views/dieselAcc/DieselAcc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Repertory">
+  <div class="DieselAcc">
     <!-- 筛选 start-->
     <!-- <div class="select-wrapper">
       <Row>
@@ -24,7 +24,7 @@
       v-model="modalObj.off"
       :title="modalObj.title"
     >
-      <Form ref="form" :label-width="50" :model="postdata" :rules="ruleInline">
+      <Form ref="form" :label-width="70" :model="postdata" :rules="ruleInline">
         <AdminRow>
           <FormItem prop="plate_num" slot="one" label="车牌">
             <AutoComplete
@@ -32,6 +32,7 @@
               v-model="postdata.plate_num"
               :data="plate_num_data"
               placeholder="请输入车牌号"
+              @on-select="plate_num_sel"
               >
             </AutoComplete>
           </FormItem>
@@ -40,9 +41,59 @@
               :filter-method="filterMethod"
               v-model="postdata.car_team"
               :data="car_team_data"
-              placeholder="请输入车牌号"
+              placeholder="请输入车队名称"
               >
             </AutoComplete>
+          </FormItem>
+        </AdminRow>
+        <AdminRow>
+          <FormItem prop="driver_name" slot="one" label="司机">
+            <AutoComplete
+              :filter-method="filterMethod"
+              v-model="postdata.driver_name"
+              :data="driver_name_data"
+              placeholder="请输入司机姓名"
+              >
+            </AutoComplete>
+          </FormItem>
+          <FormItem prop="diesel_unit_price" slot="two" label="柴油单价">
+            <AdminInputNumber
+              v-model="postdata.diesel_unit_price"
+               placeholder="请输入柴油单价"
+               @on-change="handle_diesel_unit_price_hange"
+              ></AdminInputNumber>
+          </FormItem>
+        </AdminRow>
+        <AdminRow>
+          <FormItem prop="diesel_unit" slot="one" label="柴油升数">
+            <AdminInputNumber
+              v-model="postdata.diesel_unit"
+              placeholder="请输入柴油升数"
+              @on-change="handle_diesel_unit_hange"
+              >
+            </AdminInputNumber>
+          </FormItem>
+          <FormItem prop="diesel_tot_price" slot="two" label="柴油总价">
+            <AdminInputNumber
+              v-model="postdata.diesel_tot_price"
+               placeholder="请输入柴油总价"
+               @on-change="handle_diesel_tot_price_hange"
+              ></AdminInputNumber>
+          </FormItem>
+        </AdminRow>
+        <AdminRow>
+          <FormItem prop="date" slot="one" label="加油日期">
+            <DateSelect
+              v-model="postdata.date"
+              placeholder="请输入加油日期"
+              >
+            </DateSelect>
+          </FormItem>
+          <FormItem prop="remark" slot="two" label="备注">
+            <TagsInput
+              v-model="postdata.remark"
+               placeholder="请输入备注"
+              ></TagsInput>
           </FormItem>
         </AdminRow>
       </Form>
@@ -63,9 +114,12 @@ export default {
         title: '新增'
       },
       ruleInline: {},
-      postdata: {},
+      postdata: {
+        diesel_unit_price: Number(this.$localStorage.getItem('diesel_unit_price'))
+      },
       plate_num_data: ['111', '222', '333', '444', '555', '666', '777'],
       car_team_data: ['111', '222'],
+      driver_name_data: [],
       // 筛选 start
       selectPayload: {},
       // 可选择列的 table column
@@ -93,25 +147,32 @@ export default {
     getProgramList (params) {
       let payload = Object.assign({}, this.selectPayload, params || {})
       this.$send('getAccList', {data: payload}).then((res) => {
-        console.log(res, 111)
         if (res.error === 0) {
           this.params = res.data
         } else {
           this.$Message.error('获取子程序管理失败，请稍后重试！')
         }
-      }).catch((err) => {
-        console.log(err, 222)
+      }).catch(() => {
         this.$Message.error('获取子程序管理装失败，请稍后重试！')
       })
     },
     filterMethod (value, option) {
-      console.log(value, 3223332)
-      console.log(typeof option, 21222)
-      // if (option) {
-      //   // return option.toUpperCase().indexOf(value.toUpperCase()) !== -1
-      //   return true
-      // } else { return true }
       return option.toUpperCase().indexOf(value.toUpperCase()) !== -1
+    },
+    plate_num_sel (val) {
+      console.log(val)
+    },
+    handle_diesel_unit_price_hange (val) {
+      this.$localStorage.setItem('diesel_unit_price', val)
+    },
+    handle_diesel_unit_hange (val) {
+      this.postdata.diesel_tot_price = val * this.postdata.diesel_unit_price
+    },
+    handle_diesel_tot_price_hange (val) {
+      this.postdata.diesel_unit = this.percentNum(val, this.postdata.diesel_unit_price)
+    },
+    percentNum (num, num2) {
+      return (Math.round(num / num2 * 100) / 100)
     }
   },
   activated () {
